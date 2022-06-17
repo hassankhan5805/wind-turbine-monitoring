b4a6bb1026ecfe7d01eccb83458759de243e13ec
Muhammad Hassan <hassankhan5805@gmail.com>
2022-06-17 16:26:42 +0500

    analog meter added

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_spinbox/flutter_spinbox.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -30,8 +33,8 @@ class _MyHomePageState extends State<MyHomePage> {
   bool data_ready = false;
   @override
   void initState() {
-    createCollection();
-    printData();
+    // createCollection();
+    // printData();
 
     super.initState();
   }
@@ -39,8 +42,9 @@ class _MyHomePageState extends State<MyHomePage> {
   bool doctorView = false;
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.deepPurple,
       appBar: AppBar(
         title: Text("Wind Turbine Monitoring"),
         centerTitle: true,
@@ -51,22 +55,84 @@ class _MyHomePageState extends State<MyHomePage> {
           stream: _databaseReference!.child("123").onValue,
           builder: (context, AsyncSnapshot snapshot) {
             if (snapshot.hasData) {
-              return Column(
-                children: [
-                  Row(
-                    children: [
-                      rowComponent("Temperature",
-                          "${snapshot.data.snapshot.value["temperature"].toString().split(".").first}"),
-                      rowComponent("Voltage", "${snapshot.data.snapshot.value["voltage"]}"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      rowComponent("Current",
-                          "${snapshot.data.snapshot.value["current"]}"),
-                    ],
-                  ),
-                ],
+              return Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Colors.white.withOpacity(0.1),
+                  Colors.deepPurple,
+                  Colors.black,
+                  Colors.deepPurple,
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        rowComponent("Temperature",
+                            "${snapshot.data.snapshot.value["temperature"].toString().split(".").first}\u00b0C"),
+                        rowComponent("Voltage",
+                            "${snapshot.data.snapshot.value["voltage"]}V"),
+                      ],
+                    ),
+                    Container(
+                      height: 190,
+                      width: width / 2 + 20,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.symmetric(vertical: 16, horizontal: 6),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.grey.shade300,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black45.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 3,
+                              offset: const Offset(0, 3),
+                            )
+                          ]),
+                      child: SfRadialGauge(
+                        title: GaugeTitle(
+                            text: "Current",
+                            textStyle: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17)),
+                        axes: <RadialAxis>[
+                          RadialAxis(
+                              minimum: 0,
+                              maximum: 30,
+                              ranges: <GaugeRange>[
+                                GaugeRange(
+                                    startValue: 0,
+                                    endValue: 2,
+                                    color: Colors.green),
+                                GaugeRange(
+                                    startValue: 2,
+                                    endValue: 10,
+                                    color: Colors.orange),
+                                GaugeRange(
+                                    startValue: 10,
+                                    endValue: 30,
+                                    color: Colors.red)
+                              ],
+                              pointers: <GaugePointer>[
+                                NeedlePointer(
+                                    value: double.parse(snapshot
+                                        .data.snapshot.value["current"]))
+                              ],
+                              annotations: <GaugeAnnotation>[
+                                GaugeAnnotation(
+                                    widget: Container(
+                                        child: Text(
+                                            "${snapshot.data.snapshot.value["current"]}A",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold))),
+                                    angle: 90,
+                                    positionFactor: 0.5)
+                              ])
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               );
             } else {
               return Center(
